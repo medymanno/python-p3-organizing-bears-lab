c58ced21f3ef6631aaf5fc8e26600cf1a26c1ed9
Medy <mannomedy@gmail.com>
2025-08-21 15:12:07 +0300

    add changes

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr. Chocolate", 20, "M", "brown", "calm", 1),
+("Rowdy", 10, "M", "black", "aggressive", 1),
+("Tabitha", 6, "F", "gray", "friendly", 1),
+("Sergeant Brown", 12, "M", "dark brown", "serious", 1),
+("Melissa", 8, "F", "white", "gentle", 1),
+("Grinch", 15, "M", "green", "grumpy", 1),
+("Wendy", 4, "F", "brown", "playful", 1),
+(NULL, 7, "M", "black", "mysterious", 0);
